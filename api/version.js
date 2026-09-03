4e04150e052d8ed67521238fa4fc273c84166f39
M1ntFox <tyutinevgeny@gmail.com>
2026-09-03 23:07:10 +0500

    Обновил index.html и исходный код

---
+++ api/version.js
@@ -0,0 +1,10 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.status(200).json({
+    version: '1.0.1',
+    timestamp: Date.now()
+  });
+};
